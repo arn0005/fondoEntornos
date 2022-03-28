bd310a46ba604351ac707c5e65019c05b2688189
Angel <arn0005@alu.medac.es>
2022-03-28 08:58:03 +0200

    cambio nombre proyecto

--- proyectoFondo/src/proyectoFondo/main.java
+++ proyectoFondo/src/proyectoFondo/main.java
@@ -2,19 +2,19 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package javaapplication58;
+package proyectoFondo;
 
 /**
  *
  * @author angel.ruiz
  */
-public class JavaApplication58 {
+public class main {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hola mundo");
     }
     
 }
