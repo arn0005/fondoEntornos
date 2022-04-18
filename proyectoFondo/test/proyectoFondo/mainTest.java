5abc38c7f08af6a294de873c8b9b4373502b4a23
erg0007 <erg0007@alu.medac.es>
2022-04-18 11:14:29 +0200

    Revert "test atender creado por Elena"
    This reverts commit 90900345ce4314aeeb27b4b425b953062cef4932.

---
+++ proyectoFondo/test/proyectoFondo/mainTest.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package proyectoFondo;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author angel.ruiz
+ */
+public class mainTest {
+
+    public mainTest() {
+    }
+
+    /**
+     * Test of main method, of class main.
+     */
+    @Test
+    public void testMain() {
+    }
+
+}
