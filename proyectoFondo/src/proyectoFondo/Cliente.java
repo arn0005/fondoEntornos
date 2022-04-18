958cffc0496561d4fedf4523e858a7d974fc32fc
kiko <fmo@gmail.com>
2022-04-18 10:04:03 +0200

    Adelantar y atrasar el puesto de un cliente

--- proyectoFondo/src/proyectoFondo/Cliente.java
+++ proyectoFondo/src/proyectoFondo/Cliente.java
@@ -64,9 +64,24 @@ public class Cliente {
         }
     }
 
-    public void adelantarPuesto() {
-
+  public void adelantarPuesto(Cliente adelantado) {
+    for (int i = 0; i < listaClientes.length; i++) {
+        if (adelantado.getNumLista() > listaClientes[i].getNumLista()) {
+            listaClientes[i].setNumLista(-1);
+            i = listaClientes.length;
+            adelantado.setNumLista(+1);
+        }
     }
+}
+  public void atrasarPuesto(Cliente atrasado) {
+    for (int i = 0; i < listaClientes.length; i++) {
+        if (atrasado.getNumLista() > listaClientes[i].getNumLista()) {
+            listaClientes[i].setNumLista(+1);
+            i = listaClientes.length;
+            atrasado.setNumLista(-1);
+        }
+    }
+}
 
     @Override
     public String toString() {
